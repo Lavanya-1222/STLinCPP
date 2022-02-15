4550b175c0fb4d702025c4851321051f44cd26eb
Lavanya-1222 <83878776+Lavanya-1222@users.noreply.github.com>
2022-02-15 05:09:00 -0500

    Create Array.cpp

---
+++ Array.cpp
@@ -0,0 +1,27 @@
+//this is array int stl
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+//this is array stl 
+array<int,3> arr;
+arr.fill(56);//it fill entrire with 56
+arr[2]=90;
+for(int i=0;i<arr.size();i++)cout<<arr[i]<<" ";//it works
+cout<<endl;
+for(int i=0;i<arr.size();i++)cout<<arr.at(i)<<" ";
+cout<<endl;
+for(auto i:arr)cout<<i<<" ";//its also works 
+cout<<endl;
+for(auto i=arr.begin();i!=arr.end();i++)cout<<*i<<" ";//its also works now i is pointer here
+cout<<endl;
+for(auto i=arr.rbegin();i!=arr.rend();i++)cout<<*i<<" ";// it will prin reverse
+cout<<endl;
+cout<<"size="<<arr.size();//size of array
+cout<<endl;
+cout<<"1st element="<<arr.front();//1st element
+cout<<endl;
+cout<<"last element="<<arr.back();//last element odf array
+
+}
